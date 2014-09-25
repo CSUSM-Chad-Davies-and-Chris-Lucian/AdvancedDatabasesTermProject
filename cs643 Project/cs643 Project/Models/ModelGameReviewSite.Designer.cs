8a3dd4248f1310cbce8e4605258c4e2b2e9b639b
Chris Lucian <Chris@DeadlyApps.com>
2014-09-24 17:58:03 -0700

    Starting Over with Web Forms Project

--- cs643 Project/cs643 Project/Models/ModelGameReviewSite.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Projects\School\CompSci643\cs643 Project\cs643 Project\Models\ModelGameReviewSite.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
