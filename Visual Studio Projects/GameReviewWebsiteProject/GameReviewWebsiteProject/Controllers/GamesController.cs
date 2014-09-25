24260393b870c5169f452e39c738815c309a4cd0
ChadDavies <davies.chad@gmail.com>
2014-09-24 18:53:45 -0700

    Added Games Skeleton

---
+++ Visual Studio Projects/GameReviewWebsiteProject/GameReviewWebsiteProject/Controllers/GamesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameReviewWebsiteProject.Models;
+
+namespace GameReviewWebsiteProject.Controllers
+{
+    public class GamesController : Controller
+    {
+        private GameReviewWebsiteEntities db = new GameReviewWebsiteEntities();
+
+        //
+        // GET: /Games/
+
+        public ActionResult Index()
+        {
+            return View(db.Games.ToList());
+        }
+
+        //
+        // GET: /Games/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // GET: /Games/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Games/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(game);
+        }
+
+        //
+        // GET: /Games/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // POST: /Games/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(game);
+        }
+
+        //
+        // GET: /Games/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        //
+        // POST: /Games/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
