20709abb42156c0467d354fa482eb10a047f7142
ChadDavies <davies.chad@gmail.com>
2014-10-15 19:57:59 -0700

    Added First Coded UI Tests

---
+++ Visual Studio Projects/GameReviewWebsiteProject/GameReviewWebsiteTests/GameReview.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace GameReviewWebsiteTests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class GameReview
+    {
+        public GameReview()
+        {
+        }
+
+//=========================================================================================
+        // GAME REVIEW
+        [TestMethod]
+        public void SearchForLOLReviewAndVerifyTest()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            this.UIMap.OpenSite();
+            this.UIMap.TypeInSearchBox("lol");
+            this.UIMap.SubmitLol();
+            this.UIMap.AssertLinkContent("LOL");
+            this.UIMap.closesite();
+        }
+
+        [TestMethod]
+        public void SearchForGameReviewLong()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            this.UIMap.OpenSite();
+            var Content = new String('z', 51);
+            this.UIMap.TypeInSearchBox(Content);
+            this.UIMap.SubmitLol();
+            this.UIMap.AssertLong(string.Format("Game Reviews!\r\n\r\n  \r\n\r\nYou are searching for {0}" +
+"zzzzzzzzzzzzzz\r\nSearch is limited to 50 characters \r\n\r\nGame Review Tite \r\n\r\nGame" +
+" Name \r\n\r\nRating ", Content));
+            this.UIMap.closesite();
+
+        }
+        [TestMethod]
+        public void SearchForGameReviewNothing()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            this.UIMap.OpenSite();
+            this.UIMap.SubmitLol();
+            this.UIMap.AssertEmptyGameReviewSearch();
+            this.UIMap.closesite();
+        }
+
+        [TestMethod]
+        public void SearchForGameReviewNotThere()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            this.UIMap.OpenSite();
+            this.UIMap.TypeInSearchBox("pickles34");
+            this.UIMap.SubmitLol();
+            this.UIMap.AssertWrongReview();
+            this.UIMap.closesite();
+        }
+
+//=========================================================================================
+        // END GAME REVIEW
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
---
+++ Visual Studio Projects/GameReviewWebsiteProject/GameReviewWebsiteTests/UIMap.cs
@@ -0,0 +1,45 @@
+﻿namespace GameReviewWebsiteTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Windows.Input;
+    using System.CodeDom.Compiler;
+    using System.Text.RegularExpressions;
+    using Microsoft.VisualStudio.TestTools.UITest.Extension;
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+    using MouseButtons = System.Windows.Forms.MouseButtons;
+    using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+    
+    
+    public partial class UIMap
+    {
+        public void TypeInSearchBox(string content)
+        {
+            #region Variable Declarations
+            HtmlEdit uISearchEdit = this.UIGameReviewsGameRevieWindow1.UIGameReviewsGameRevieDocument.UISearchEdit;
+            #endregion
+
+            // Type 'lol' in 'search' text box
+            uISearchEdit.Text = content;
+        }
+        public void AssertLinkContent(string content)
+        {
+            #region Variable Declarations
+            HtmlHyperlink uILOLHyperlink = this.UIGameReviewsGameRevieWindow1.UIGameReviewsGameRevieDocument.UIBodyPane.UILOLHyperlink;
+            #endregion
+
+            // Verify that the 'InnerText' property of 'LOL' link equals 'LOL'
+            Assert.AreEqual(content, uILOLHyperlink.InnerText);
+        }
+        public void AssertLong(string content)
+        {
+            string UIItemCustomInnerText = "Game Reviews!\r\n\r\n  \r\n\r\nYou are searching for zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz" +
+         "zzzzzzzzzzzzzz\r\nSearch is limited to 50 characters \r\n\r\nGame Review Tite \r\n\r\nGame" +
+         " Name \r\n\r\nRating ";
+        }
+    }
+}
