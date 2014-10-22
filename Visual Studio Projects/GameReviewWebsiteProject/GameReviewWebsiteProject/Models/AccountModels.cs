31a22b81d7aa28b1cbb8a1fe6bc79029aa01ccec
Chris Lucian <Chris@DeadlyApps.com>
2014-10-22 12:24:26 -0700

    Added String Length to Registration Model
    Fixed errors with registration page when fields are populated with too
    many characters

--- Visual Studio Projects/GameReviewWebsiteProject/GameReviewWebsiteProject/Models/AccountModels.cs
+++ Visual Studio Projects/GameReviewWebsiteProject/GameReviewWebsiteProject/Models/AccountModels.cs
@@ -72,18 +72,21 @@ namespace GameReviewWebsiteProject.Models
 
     public class RegisterModel
     {
+
+        [StringLength(150)]
         [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(10)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
+        [StringLength(10)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
